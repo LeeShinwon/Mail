2d4b783e101f049013043269912dd0394b9ef671
LeeShinwon <leeshin1130@gmail.com>
2022-06-30 17:35:30 +0900

    sent 메일 수정

---
+++ lib/mail/sent_mail/sent_mail_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SentMailList extends StatefulWidget {
+  const SentMailList({Key? key}) : super(key: key);
+
+  @override
+  State<SentMailList> createState() => _SentMailListState();
+}
+
+class _SentMailListState extends State<SentMailList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
