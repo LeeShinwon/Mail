f4a5a4118f0052c7413cfb2a61f3ff00072218c8
Lab <seoye6618@gmail.com>
2022-06-30 15:41:57 +0900

    [FEAT] fill home.dart whit new contents

--- lib/home.dart
+++ lib/home.dart
@@ -4,29 +4,64 @@ import 'package:flutter/material.dart';
 import 'login.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (BuildContext context, AsyncSnapshot snapshot){
-              if(!snapshot.hasData){
-                return const LoginPage();
-              }else{
-                return Center(
-                  child: Column(
-                    children: [
-                      SizedBox(height: 100,),
-                  Text("${snapshot.data.displayName}님 환영합니다.", style: TextStyle(
-                    fontSize: 20
-                  ),)
-                ],
+        body: SafeArea(
+      child: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (!snapshot.hasData) {
+              return const LoginPage();
+            } else {
+              return Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "편집",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 22,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "메일상자",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 35,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(16),
+                      child: Column(
+                        children: [
+
+                        ],
+                      ),
+                    ),
+                    Text(
+                      "${snapshot.data.displayName}님 환영합니다.",
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
                 ),
-                );
-              }
-            })
-    );
+              );
+            }
+          }),
+    ));
   }
 }
