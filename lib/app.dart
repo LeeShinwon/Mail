1ce47df87a8e6830f9a8f767487f4c58e3973e07
LeeShinwon <leeshin1130@gmail.com>
2022-06-30 16:59:16 +0900

    메일 관련 파일들을 만들었음

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mail/login.dart';
 
 import 'home.dart';
 
