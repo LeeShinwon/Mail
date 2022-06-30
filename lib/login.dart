fd39887f7d7813aee05d9eca5e0ac953f87a5ffc
Lab <seoye6618@gmail.com>
2022-06-30 14:52:14 +0900

    [FEAT] fix error and fill in login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'home.dart';
 
 class CheckAuth extends StatefulWidget {
   const CheckAuth({Key? key}) : super(key: key);
@@ -26,13 +31,122 @@ class _CheckAuthState extends State<CheckAuth> {
 }
 
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+
+  CollectionReference database = FirebaseFirestore.instance.collection('user');
+  late QuerySnapshot querySnapshot;
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+    await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal:
+        10//getScreenWidth(context)*0.1
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xFFEFEFEF),
+
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100//getScreenHeight(context) * 0.15,
+            ),
+            Text(
+              "Mail",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 35),
+            ),
+            SizedBox(
+              height: 200//getScreenHeight(context) * 0.14,
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final UserCredential userCredential = await signInWithGoogle();
+
+                User? user = userCredential.user;
+
+                if (user != null) {
+                  int i;
+                  querySnapshot = await database.get();
+
+                  for (i = 0; i < querySnapshot.docs.length; i++) {
+                    var a = querySnapshot.docs[i];
+
+                    if (a.get('uid') == user.uid) {
+                      break;
+                    }
+                  }
+
+                  if (i == (querySnapshot.docs.length)) {
+                    database.doc(user.uid).set({
+                      'email': user.email.toString(),
+                      'name': user.displayName.toString(),
+                      'uid': user.uid,
+                    });
+                  }
+                  if (user != null)
+                    Get.to(CheckAuth());
+                }
+              },
+              child: Row(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    child: Image.asset('assets/image/google_logo.PNG'),),
+                  SizedBox(
+                    width: 50,
+                  ),
+                  Text(
+                    '구글 로그인',
+                    style: TextStyle(
+                      fontSize: 25.0,
+                      color: Color(0xff000000),
+                    ),
+                  ),
+                ],
+              ),
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(
+                    80, 30
+                      //getScreenWidth(context)*0.88, getScreenHeight(context)*0.1
+                  ),
+                  primary: Color(0xFFffffff),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  elevation: 10
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
