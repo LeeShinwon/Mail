2045a532c55d46ee72afb2e744a5ce97468ba353
LeeShinwon <leeshin1130@gmail.com>
2022-07-07 00:38:55 +0900

    step3에 추가한 내용 추가

--- lib/mail/mail_content/show_mail.dart
+++ lib/mail/mail_content/show_mail.dart
@@ -12,19 +12,99 @@ class ShowMail extends StatelessWidget {
       appBar: AppBar(
         title: Text("상세 메일",
           style: TextStyle(color: Colors.black, fontSize: 15, fontWeight: FontWeight.w600),),
-        //배경 색이랑 동일하게 적용해주기
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         iconTheme: IconThemeData(
           color: Colors.black,//색변경
           size: 15,
         ),
       ),
+
       body: ListView(
         padding: const EdgeInsets.fromLTRB(30, 10,0,0),
         children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(CupertinoIcons.circle_fill, size: 40,color: Colors.grey,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width:MediaQuery.of(context).size.width - 85,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(mailDoc!.writer,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                ),),
+                              Text(mailDoc!.time+".",style: TextStyle(
+                                color: Colors.grey,
+                              )),
+                            ],
 
-        ]
+                          ),
+                        ),
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Text("받는 사람: ",),
+                            Text(mailDoc!.writer+" ",
+                              style: TextStyle(
+                                color: Colors.grey,
+                              ),),
+                            Icon(CupertinoIcons.chevron_right , size:12,color: Colors.grey,),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                  ),
+
+
+                ],
+              ),
+
+              Divider(color: Colors.black45,),
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(mailDoc!.title,style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20,
+                  ),),
+                  Text(mailDoc!.content,)
+                ],
+              ),
+
+
+            ],
+          ),
+        ],
       ),
+      bottomNavigationBar:BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.archivebox , color: Colors.blueAccent,)),
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.folder, color: Colors.blueAccent,)),
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.arrowshape_turn_up_left , color: Colors.blueAccent,)),
+            Spacer(),
+            IconButton(onPressed: (){}, icon: Icon(CupertinoIcons.pencil_outline, color: Colors.blueAccent,)),
+
+
+          ],
+        ),
+      ) ,
     );
   }
 }
--- lib/mail/mail_list/mail_list.dart
+++ lib/mail/mail_list/mail_list.dart
@@ -4,11 +4,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:mail/mail/mail_content/show_mail.dart';
+import 'package:mail/mail/mail_content/temporaryStorage_modal.dart';
 
 import '../../util/size.dart';
 import '../mail.dart';
-import '../mail_content/show_mail.dart';
-import '../mail_content/temporaryStorage_modal.dart';
+import '../sendMail_modal.dart';
 import 'mail_card.dart';
 
 class MailList extends StatefulWidget {
@@ -22,6 +23,7 @@ class MailList extends StatefulWidget {
 class _MailListState extends State<MailList> {
   final user = FirebaseAuth.instance.currentUser;
 
+  //final userCollectionReference = FirebaseFirestore.instance.collection('user');
   late QuerySnapshot querySnapshot;
 
 
@@ -31,7 +33,6 @@ class _MailListState extends State<MailList> {
       stream: FirebaseFirestore.instance.collection('mail').snapshots(),
       builder: (BuildContext context,
           AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-        //아직 데이터가 다 가져와지지 않았다면 파란색 동그라미가 보여짐
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(), //로딩되는 동그라미 보여주기
@@ -45,23 +46,39 @@ class _MailListState extends State<MailList> {
         if (snapshot.hasData) {
           for (int i = 0; i < snapshot.data!.docs.length; i++) {
             var one = snapshot.data!.docs[i];
-
-            //'메일을 보낸 사람이나 받은 사람 중에 현재 사용자가 해당한다면' mailDocs list에 담아준다.
             if (one.get('writer') == user!.email ||
                 one.get('recipient') == user!.email) {
 
-              Timestamp t = one.get('time');//파이어베이스에서 time은 메일이 보내진 시간을 의미하는데, Timestamp 형태로 저장되기 때문에 형변환이 필요함
-              print(one.get('time'));//console에서 보기
+              Timestamp t = one.get('time');
+              print(one.get('time'));
               String time = DateTime.fromMicrosecondsSinceEpoch(t.microsecondsSinceEpoch).toString().split(" ")[0];
-              //2022-07-04 10:45:45.473999 이런 형태라서, ' '를 기준으로 쪼개서 앞에 날짜만 가져옵니다.
-              print(DateTime.fromMicrosecondsSinceEpoch(t.microsecondsSinceEpoch).toString());
               print(time);
               time = time.replaceAll("-", ".");
 
-              //Mail 생성자를 이용해 데이터를 담은 객체를 만들고 list에 추가 시켜준다.
               Mail mail = Mail(one.id,one.get('title'), one.get('content'), one.get('writer'), one.get('recipient'), time, one.get('read'), one.get('sent'));
-              mailDocs.add(mail);
 
+              if (widget.title == "보낸 편지함") {
+                if (one.get('writer') == user!.email) {
+                  //trick: 내가 보낸 편지는 내가 열어볼때 read 값이 false이어도 이미 읽은 것으로 보여주기 위한
+                  Mail mymail = Mail(one.id,one.get('title'), one.get('content'), one.get('writer'), one.get('recipient'), time, true, one.get('sent'));
+                  mailDocs.add(mymail);
+                }
+              }
+              else if (widget.title == "받은 편지함") {
+                if (one.get('recipient') == user!.email) {
+                  mailDocs.add(mail);
+                }
+              }
+              else if (widget.title == "임시 저장") {
+                if (one.get('writer') == user!.email &&
+                    one.get('sent') == false) {
+                  mailDocs.add(mail);
+                }
+              }
+              else if (widget.title == "모든 메일") {
+                mailDocs.add(mail);
+              }
+              else {}
             }
           }
         }
@@ -70,10 +87,12 @@ class _MailListState extends State<MailList> {
         return ListView.builder(
             itemCount: mailDocs.length,
             itemBuilder: (context, index) {
-
-              //사용자가 클릭하면 반응함.
               return GestureDetector(
                 onTap: () {
+                  //받은 사람이 메일을 읽으려고 onTap을 하면 파이어베이스의 mail - read 부분을 true로 update 해주어야 함
+                  if(mailDocs[index].recipient == user?.email){
+                    FirebaseFirestore.instance.collection('mail').doc(mailDocs[index].mail_id).update({'read':true});
+                  }
                   widget.title == "임시 저장" ?
                   showModalBottomSheet( //reference : https://api.flutter.dev/flutter/material/showModalBottomSheet.html
                       isScrollControlled: true,
@@ -90,7 +109,6 @@ class _MailListState extends State<MailList> {
                   ) :
                   Get.to(ShowMail(mailDocs[index]));
                 },
-                //itemCount 갯수만큼, MailCard 를 가져와서 보여줌.
                 child: MailCard(mailDocs[index]),
               );
             }
